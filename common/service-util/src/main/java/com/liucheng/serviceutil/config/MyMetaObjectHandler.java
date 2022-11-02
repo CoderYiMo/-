c65fbd0ccb018d6f75fc2d11620b65bd51fc9318
liucheng <lc13789090753@163.com>
2022-11-02 17:01:56 +0800

    初始化项目，搭建项目框架

---
+++ common/service-util/src/main/java/com/liucheng/serviceutil/config/MyMetaObjectHandler.java
@@ -0,0 +1,26 @@
+package com.liucheng.serviceutil.config;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * @author 刘呈
+ * @createTime 2022/11/2 16:13
+ * @description mp的自动填充功能
+ */
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.setFieldValByName("createTime", new Date(), metaObject);
+        this.setFieldValByName("updateTime", new Date(), metaObject);
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+
+    }
+}
